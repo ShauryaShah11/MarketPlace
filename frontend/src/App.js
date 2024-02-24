cb7e0e2c0b10c313690f79df3818eb23cc985c61
rajankumar <rajankumar77915@gmail.com>
2024-02-24 18:25:28 +0530

    perticular product detail template created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Login from './Login';
+import ProductDetail from './pages/ProductDetail';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
        
         <Route path='/' element={<Login/>}></Route>
+        <Route path='/ProductDetail' element={<ProductDetail/>}></Route>
            
         </Routes>
       </Router>
