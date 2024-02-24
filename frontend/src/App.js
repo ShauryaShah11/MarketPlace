cc0098ef5cfcbc764481c76a7f637f72e391b478
danger9912 <129598732+danger9912@users.noreply.github.com>
2024-02-24 11:29:25 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
-import Login from './Components/login';
+import Login from './Login';
+
+
 function App() {
   return (
     <>
       <Router>
 
         <Routes>
-        <Route path="/" element={<Login />} />
+       
+        <Route path='/' element={<Login/>}></Route>
            
         </Routes>
       </Router>
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,107 @@
+import { IoCloseSharp } from "react-icons/io5";
+// import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+// import { setCookie } from "cookies-next";
+// import { useRouter } from 'next/navigation'
+// import { useDispatch, useSelector } from "react-redux";
+// import { login } from '../../services/functions/login'
+// import IconBtn from '../componant/IconBtn'
+import { FaGoogle } from "react-icons/fa";
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginInProgress, setLoginInProgress] = useState(false);
+//   const router = useRouter();
+//   const dispatch = useDispatch();
+//   const token = useSelector((state) => state?.auth?.token);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      switch (e.target.name) {
+        case "email":
+          document?.getElementById("password")?.focus();
+          break;
+        case "password":
+        //   handleLogin();
+          document?.getElementById("password")?.blur()
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+//   const handleFormSubmit = async (ev) => {
+//     ev.preventDefault();
+//     setLoginInProgress(true);
+//     try {
+//     //   dispatch(login(email, password, router.push));
+//     } catch (error) {
+//       console.error('Error during login:', error);
+//     }
+//     setLoginInProgress(false);
+//   };
+
+
+  return (
+    <>
+      <div className="w-full  flex justify-between border border-none bg-richblack-200">
+        <div className="mt-8 w-full flex flex-col justify-center items-center">
+          <div className="rounded-lg">
+            <img
+              className="rounded-md h-auto mb-2"
+              alt="logo"
+              src="./4.png"
+              width={200}
+              height={10}
+            />
+          </div>
+
+          <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+          <form className="w-10/12 mx-auto " >
+            <div className="ml-16">
+
+              <label htmlFor="email">Email:</label><br></br>
+              <input
+                className="h-10  rounded-md border-r-2 pl-2 w-10/12 mb-3"
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Email/Mobile"
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
+              <br></br><label htmlFor="password">Password:</label><br></br>
+              <input
+                className="h-10 rounded-md border-r-2 pl-2 w-10/12 mb-6"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+              {/* <IconBtn text={"Login"} customClasses={"justify-center w-10/12"}></IconBtn> */}
+            </div>
+            <div className="my-4 text-center text-gray-500 ">or login with provider</div>
+            <div className="flex justify-center">
+              <button
+                type="button"
+               
+                className="flex items-center gap-2 justify-center mb-6 bg-blue-500  text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+              >
+                <FaGoogle />
+                <span>Login with Google</span>
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="w-10/12 border border-none p-0">
+         <img src="https://e1.pxfuel.com/desktop-wallpaper/55/1001/desktop-wallpaper-3d-login-page.jpg"/>
+        </div>
+      </div>
+    </>
+  );
+}
